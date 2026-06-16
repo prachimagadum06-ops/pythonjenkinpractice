3b7bc856f5446f226f2caaf76e987e74ff5384fb
prachimagadum06 <prachimagadum06@gmail.com>
2026-06-16 20:20:38 +0530

    add data

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data= [
+        {"name": "John","age": 25,"city": "Mumbai"},
+        {"name": "Suja","age": 40,"city": "Mumbai"},
+        {"name": "Prachi","age": 20,"city": "Pune"},
+        {"name": "uday","age": 45,"city": "Pune"}
+    ]
+
+    return render_template("index.html", user=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
